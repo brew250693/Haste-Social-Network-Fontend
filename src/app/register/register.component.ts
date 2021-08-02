4aadeae8aa2f3771b6bb642e70b1befe6ba7739f
brew250693 <brew250693@gmail.com>
2021-08-02 12:55:27 +0700

    Add login

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: any = {
+    username: null,
+    password: null,
+    email: null,
+    phone: null,
+    birthday: null,
+    city: null,
+
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const { username, email, password, phone, birthday, city } = this.form;
+
+    this.authService.register(username, email, password, phone, birthday, city).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+  }
+}
